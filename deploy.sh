bb7f7e2297b80645ce3b24af9b5e88f5b525109a
njlr <njlr@users.noreply.github.com>
2020-12-30 19:24:38 +0000

    * Tweaks deploy process

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,32 @@ dotnet restore
 dotnet tool restore
 dotnet paket restore
 
+cd ./app
+
+git clone git@github.com:bikehackers/bikehackers.github.io.git ./public
+
+cd ./public
+
+git fetch origin
+# git checkout -b master origin/master
+
+rm -r *
+
+cd ../..
+
 dotnet fsi ./BuildTyres.fsx
 
 cd ./app
 
-yarn webpack -c Release
+yarn install --pure-lockfile
+NODE_ENVIRONMENT=production yarn webpack
 
 cd ./public
 
-git init
-git remote add origin git@github.com:bikehackers/bikehackers.github.io.git
+pwd
+git status
+git remote -v
 
 git add .
 git commit -m 'Deploy'
-git pull --set-upstream origin master
-git push --force
+git push
