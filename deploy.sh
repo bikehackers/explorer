4f145a2321524a26b56770faa17a68c31549eba2
njlr <njlr@users.noreply.github.com>
2021-01-01 18:03:41 +0000

    * Disables deploy for now

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,9 @@ cd ./app
 git config --global user.email 'bikehackers@users.noreply.github.com'
 git config --global user.name bikehackers
 
+pwd
 # git clone git@github.com:bikehackers/bikehackers.github.io.git ./public
-git clone --branch master "ssh://$GITHUB_TOKEN@github.com/bikehackers/bikehackers.github.io.git" ./public
+git clone --branch master "ssh://$GITHUB_TOKEN@github.com/bikehackers/bikehackers.github.io.git" ./public  || exit 1
 
 cd ./public
 
@@ -20,13 +21,13 @@ git fetch origin
 rm -r *
 
 cd ../..
-dotnet fsi ./BuildTyres.fsx
+dotnet fsi ./BuildTyres.fsx || exit 1
 
 cd ./app
 pwd
 
 yarn install --pure-lockfile
-NODE_ENVIRONMENT=production yarn webpack
+NODE_ENVIRONMENT=production yarn webpack || exit 1
 
 cd ./public
 
