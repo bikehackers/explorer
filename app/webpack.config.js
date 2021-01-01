89596db90d75e3fb4be34d6f6cda5c0948a02945
njlr <njlr@users.noreply.github.com>
2021-01-01 17:37:47 +0000

    * Re-adds app

---
+++ app/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const mode = process.env["NODE_ENVIRONMENT"] === "production" ? "production" : "development"
+
+console.log({ mode });
+
+module.exports = {
+  mode,
+  entry: "./app.fsproj",
+  output: {
+    path: path.join(__dirname, "./public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./public",
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.fs(x|proj)?$/,
+        use: "fable-loader"
+      }
+    ]
+  },
+  plugins: [ new HtmlWebpackPlugin({ template: "index.ejs" }) ]
+}
