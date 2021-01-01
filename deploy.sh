bec29dea053d15329837a17d7ffcfe92b5628b63
njlr <njlr@users.noreply.github.com>
2021-01-01 17:26:43 +0000

    * Use SSH for deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ git config --global user.email 'bikehackers@users.noreply.github.com'
 git config --global user.name bikehackers
 
 # git clone git@github.com:bikehackers/bikehackers.github.io.git ./public
-git clone --branch master "https://$GITHUB_TOKEN@github.com/bikehackers/bikehackers.github.io.git" ./public
+git clone --branch master "ssh://$GITHUB_TOKEN@github.com/bikehackers/bikehackers.github.io.git" ./public
 
 cd ./public
 
